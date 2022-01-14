c0fd4d5e18986b32be6cec5df50334f24afb6f7f
Zach Beckwith <zbeckwith6@gmail.com>
2022-01-13 18:54:30 -0600

    Testimonials Start

--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -1,27 +1,11 @@
 const testimonials = [
   {
     id: 1,
-    name: 'Shaif Arfan',
-    title: 'CEO',
-    org: 'WEB CIFAR',
+    name: 'Will Grbcich',
+    title: 'Real Estate Agent',
+    org: 'Hermann London',
     desc:
-      'Ayan is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
-  },
-  {
-    id: 2,
-    name: 'Fahim Elahe',
-    title: 'Co-founder',
-    org: 'Kreetive',
-    desc:
-      'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
-  },
-  {
-    id: 3,
-    name: 'Sayed',
-    title: 'Marketing Head',
-    org: 'Eleven Dawn',
-    desc:
-      'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+      'I am extremely please with the website Zach built for me. It was everything that I could have hoped for and more. He was a true professional during his time working on my site',
   },
 ];
 export default testimonials;
